38b954d272b119ab30d91eb0269caa7763cfbcfa
infermo <80454332+infermo@users.noreply.github.com>
2021-10-23 03:34:38 +0300

    Вроде что то заработало, но это не точно
    3:34

--- bastion.py
+++ bastion.py
@@ -1,19 +1,23 @@
 from paho.mqtt import client as mqtt_client
 import random
 import time
+import json
 
 broker = 'mqtt0.bast-dev.ru'
 port = 1883
-topic_prefix = "service/weather_logger"
+topic_prefix = 'service/weather_logger'
 username = 'hackathon'
 password = 'Autumn2021'
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
 def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+    def on_connect(client, userdata, flags, rc):    
+        time.sleep(1)
+        # if rc == 0:
+        #    print("Connected to MQTT Broker!")
+        # else:
+        #    print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
@@ -30,20 +34,28 @@ def publish(client):
         result = client.publish(topic_prefix, msg)
         result: [0, 1]
         status = result[0]
-        #if status == 0:
-        #    print(f"Send `{msg}` to topic `{topic_prefix}`")
-        #else:
-        #    print(f"Failed to send message to topic {topic_prefix}")
-        #msg_count += 1
+zalupa = []
 
-def on_message(client,userdata,message):
-    print('Message:', str(message.payload.decode("utf-8")))
+
+def on_message(client, userdata, message):
+    #print('Message:', str(message.payload.decode("utf-8")))
+    zalupa.append(message.payload)
+    print(message.topic,':', (message.payload))
+    print(zalupa)
+
+
 def run():
+    zalupa.clear()
     client = connect_mqtt()
+
     client.loop_start()
+    #client.subscribe([('service/weather_logger/outdoor_temperature', 0), ('service/weather_logger/soil_humidity', 0),
+    #                  ('service/weather_logger/soil_temperature', 0)])
+    client.subscribe('service/weather_logger/outdoor_temperature')
     client.subscribe('service/weather_logger/soil_humidity')
+    client.subscribe('service/weather_logger/soil_temperature')
+    client.subscribe('service/weather_logger/outdoor_humidity')
+    client.subscribe('service/weather_logger/outdoor_light')
     client.on_message = on_message
     publish(client)
-    time.sleep(1)
-
 run()
